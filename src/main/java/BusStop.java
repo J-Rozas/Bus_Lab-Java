7bfe378c8f6613da55c700c2a4fd1a6825694ea9
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-14 17:55:14 +0100

    Add constructor to BusStop

--- src/main/java/BusStop.java
+++ src/main/java/BusStop.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class BusStop {
+
+    private final String name;
+    private ArrayList<Person> queue;
+
+    public BusStop(String name) {
+        this.name = name;
+        this.queue = new ArrayList<>();
+    }
 }
