ecd233b4963b009f397a7b852759f1d0c7f3e404
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-14 18:10:26 +0100

    Add test to check that one person can be added to queue

--- src/test/java/BusStopTest.java
+++ src/test/java/BusStopTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 public class BusStopTest {
     private BusStop busStop;
+    private Person person;
 
     @Before
     public void before() {
         busStop = new BusStop("Random stop");
+        person = new Person("Joe");
     }
 
     @Test
@@ -19,5 +21,11 @@ public class BusStopTest {
     @Test
     public void hasEmptyQueueAtFirst() {
         assertEquals(0, busStop.getQueue());
+    }
+
+    @Test
+    public void canAddPersonToQueue() {
+        busStop.addPerson(person);
+        assertEquals(1, busStop.getQueue());
     }
 }
