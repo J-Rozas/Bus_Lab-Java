b494a5428f4e6e9ee116b361ca2c189cec68fa7a
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-14 18:14:52 +0100

    Add test to check that a person can be removed from queue

--- src/test/java/BusStopTest.java
+++ src/test/java/BusStopTest.java
@@ -6,11 +6,13 @@ import static org.junit.Assert.assertEquals;
 public class BusStopTest {
     private BusStop busStop;
     private Person person;
+    private Person person2;
 
     @Before
     public void before() {
         busStop = new BusStop("Random stop");
         person = new Person("Joe");
+        person2 = new Person("Dave");
     }
 
     @Test
@@ -26,6 +28,14 @@ public class BusStopTest {
     @Test
     public void canAddPersonToQueue() {
         busStop.addPerson(person);
+        assertEquals(1, busStop.getQueue());
+    }
+
+    @Test
+    public void canRemovePersonFromQueue() {
+        busStop.addPerson(person);
+        busStop.addPerson(person2);
+        busStop.remove(person);
         assertEquals(1, busStop.getQueue());
     }
 }
